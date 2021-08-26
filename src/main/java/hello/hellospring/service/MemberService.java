33099d34ce7ed201e48bf44b5395a0cc0130b426
sonjinyoung <handsun000@gmail.com>
2021-08-26 18:16:42 +0900

    member inquiry view

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -2,6 +2,8 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
@@ -35,7 +37,7 @@ public class MemberService {
     /**
      * 전체 회원 조회
      */
-    public List<Member> findMember(){
+    public List<Member> findMembers(){
         return memberRepository.findAll();
     }
 
